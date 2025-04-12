eb237ad45b1f6df2448469ace9d6064ec16c1c5c
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-04-12 15:45:46 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-string name, temp, result;
+string name, result;
 int cnt[26];
-int oddNum, oddIdx;
+int oddNum;
 
 int main()
 {
@@ -14,16 +12,17 @@ int main()
 	cout.tie(0);
 
 	cin >> name;
-	sort(name.begin(), name.end());
+
 	for (auto i : name)
 		cnt[i - 'A']++;
 
 	for (int i = 0; i < 26; i++)
 	{
+		// 홀수표현: cnt[i] & 1 (비트연산자)
 		if (cnt[i] && cnt[i] % 2 == 1)
 		{
+			result += (i + 'A');
 			oddNum++;
-			oddIdx = i;
 		}
 	}
 
@@ -31,22 +30,18 @@ int main()
 		cout << "I'm Sorry Hansoo" << '\n';
 	else
 	{
-		if (oddNum)
-			result += (oddIdx + 'A');
-
 		for (int i = 25; i >= 0; i--)
 		{
 			if (cnt[i])
 			{
 				int half = cnt[i] / 2;
-				string s(half, char(i + 'A'));
-				result = s + result + s;
+				string temp(half, char(i + 'A'));
+				result = temp + result + temp;
 			}
 		}
 
 		cout << result << '\n';
 	}
-
 
 	return 0;
 }
