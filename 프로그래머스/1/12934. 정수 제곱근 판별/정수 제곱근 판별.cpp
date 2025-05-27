f4bb47f1bfad922489cf9d6fae826ba607b833c6
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-05-27 22:01:31 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -8,6 +8,6 @@ long long solution(long long n) {
     long long answer = 0;
     
     int k = sqrt(n);
-    if((long long)k*k==n) return (long long)(k+1)*(k+1);
+    if(static_cast<long long>(k)*k==n) return static_cast<long long>(k+1)*(k+1);
     else return -1;
 }
