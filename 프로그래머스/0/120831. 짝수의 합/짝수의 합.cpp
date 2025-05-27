a521dc8f2abea9239d94bc4d3f43102ab4be4eb6
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-05-27 11:59:42 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -6,11 +6,17 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
     
+    /*
     for(int i=0; i<=n; i++)
     {
         if(i%2==0)
             answer+=i;
     }
+    */
+    
+    // 등차수열의 합 공식: 항의 갯수 * (첫 항 + 막 항) / 2
+    int count = n/2;
+    answer = count*(2+count*2)/2;
     
     return answer;
 }
