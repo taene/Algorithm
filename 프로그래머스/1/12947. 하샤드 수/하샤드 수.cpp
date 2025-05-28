9df35cf30d19d9a3288be8e5fab13c4ee16bf87d
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-05-28 11:51:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    
+    int temp = x;
+    int sum = 0;
+    while(temp)
+    {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    
+    return (x % sum == 0);
+}
