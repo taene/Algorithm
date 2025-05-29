537c4e35ffafb292c44468eb8bdf19fff8c06e91
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-05-29 11:12:09 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    if(s.size()&1) answer = s.substr(s.size()/2, 1);
+    else answer = s.substr(s.size()/2-1, 2);
+    
+    return answer;
+}
