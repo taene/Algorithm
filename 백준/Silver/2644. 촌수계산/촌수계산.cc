dbf6ea9fac99e2781527c3c5c1cf2115d168b877
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-11-20 10:44:25 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v[104];
+bool visited[104];
+int a, b;
+int mn = -1;
+
+void dfs(int here, int cnt)
+{
+    visited[here] = true;
+    if (here == b)
+    {
+        if (mn == -1 || cnt < mn)
+        {
+            mn = cnt;
+        }
+        return;
+    }
+
+    for (auto i : v[here])
+    {
+        if (!visited[i])
+        {
+            dfs(i, cnt + 1);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n;
+    cin >> a >> b;
+    cin >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        int x, y;
+        cin >> x >> y;
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
+    dfs(a, 0);
+    cout << mn;
+
+    return 0;
+}
