9d1343e6eba97d5f380d35bfed01a28772c251c0
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-11-26 10:39:27 +0900

    Update 유기농 배추.cc

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,64 +1,65 @@
 #include <iostream>
-#include <memory.h>
+
 using namespace std;
 
-bool arr[51][51] = { false };
-bool visit[51][51] = { false };
-int dx[4] = { 0,0,-1,1 };
-int dy[4] = { -1,1,0,0 };
-int m, n, k;	//배추밭 가로, 세로, 개수
+int m, n, k;
+int arr[50][50];
+bool visited[50][50];
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+int ret;
 
 void dfs(int y, int x)
 {
-	visit[y][x] = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (arr[ny][nx] == 1 && visit[ny][nx] == 0
-			&& (ny >= 0 && ny < n) && (nx >= 0 && nx < m))
-		{
-			dfs(ny, nx);
-		}
-	}
+    visited[y][x] = true;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+        if (visited[ny][nx] == true || arr[ny][nx] == 0) continue;
+
+        dfs(ny, nx);
+    }
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		for (int i = 0; i < 51; i++) 
-		{
-			memset(visit[i], 0, sizeof(bool) * (51));	//함수 메모리 초기화
-			memset(arr[i], 0, sizeof(bool) * (51));
-		}
-		cin >> m >> n >> k;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ret = 0;
+        fill(&arr[0][0], &arr[49][50], 0);
+        fill(&visited[0][0], &visited[0][0] + 50 * 50, false);
 
-		while (k--)
-		{
-			int x, y;	//배추 위치
-			cin >> x >> y;
-			arr[y][x] = 1;
-		}
+        cin >> m >> n >> k;
+        for (int i = 0; i < k; ++i)
+        {
+            int x, y;
+            cin >> x >> y;
+            arr[y][x] = 1;
+        }
 
-		int cnt = 0;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				if (arr[i][j] == 1 && visit[i][j] == 0)
-				{
-					dfs(i, j);
-					cnt++;
-				}
-			}
-		}
-		cout << cnt << '\n';
-	}
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (!visited[i][j] && arr[i][j] == 1)
+                {
+                    ret++;
+                    dfs(i, j);
+                }
+            }
+        }
+        cout << ret << '\n';
+    }
+
+    return 0;
 }
