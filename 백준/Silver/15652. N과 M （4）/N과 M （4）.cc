1ed85f5b05a63fd313cd8fe6a96cca20fad03493
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-12-08 10:14:03 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+void dfs(int here)
+{
+    if (v.size() == m)
+    {
+        for (auto i : v)
+        {
+            cout << i << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = here; i <= n; ++i)
+    {
+        v.push_back(i);
+        dfs(i);
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    dfs(1);
+
+    return 0;
+}
