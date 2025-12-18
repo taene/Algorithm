8aa66f348c767033851ae53c4eac29026616f5d6
TaeYeonKim <81561694+taene@users.noreply.github.com>
2025-12-18 10:09:58 +0900

    Update 설탕 배달.cc

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int N;
-int main() {
-	cin >> N;
-	int ans = 0;
-	while (N >= 0) {
-		if (N % 5 == 0) {	//가장 큰 수로 나누는게 가장 작은수랑 섞어서 나누는 것보다 유리
-			ans += (N / 5);	//나눈 몫을 더한 것이 정답
-			cout << ans << endl;
-			return 0;
-		}
-		N -= 3;	//3kg을 빼고 
-		ans++;	//가방 하나 늘어남
-	}
-	cout << -1 << endl;
+int n;
+int cnt = -1;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = n / 5; i >= 0; --i)
+    {
+        int rest = n - (5 * i);
+        if (rest % 3 == 0)
+        {
+            cnt = i + (rest / 3);
+            break;
+        }
+    }
+    cout << cnt;
+
+    return 0;
 }
