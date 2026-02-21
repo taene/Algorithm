aea622df5075cd602fd17d0152d71a895304dfeb
TaeYeonKim <81561694+taene@users.noreply.github.com>
2026-02-22 01:07:29 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,52 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <map>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n, c;
-map<int, int> mp_cnt;
-map<int, int> mp_idx;
-vector<pair<int, int>> ret;
+int N,C;
+map<int,int> mpCnt; // num/cnt
+map<int,int> mpIdx; // num/Idx
+vector<pair<int,int>> ret;  // num/cnt
 
-bool cmp(pair<int, int> a, pair<int, int> b)
+bool cmp(pair<int,int> a, pair<int,int> b)
 {
-	if (a.second == b.second)
-		return mp_idx[a.first] < mp_idx[b.first];
-	else
-		return a.second > b.second;
+    if(a.second==b.second)
+        return mpIdx[a.first]<mpIdx[b.first];
+    else
+        return a.second>b.second;
 }
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> n >> c;
-	
-	for (int i = 0; i < n; i++)
-	{
-		int temp;
-		cin >> temp;
-		mp_cnt[temp]++;
-
-		if (mp_idx.find(temp) == mp_idx.end())
-			mp_idx[temp] = i;
-	}
-
-	for (auto it : mp_cnt)
-	{
-		ret.push_back({ it.first, it.second });
-	}
-
-	sort(ret.begin(), ret.end(), cmp);
-
-	for (auto it : ret)
-	{
-		for (int i = 0; i < it.second; i++)
-			cout << it.first << ' ';
-	}
-
-	return 0;
+    cin>>N>>C;
+    
+    for(int i=0; i<N; ++i)
+    {
+        int temp;
+        cin>>temp;
+        mpCnt[temp]++;
+        
+        if(mpIdx.find(temp) == mpIdx.end())
+            mpIdx[temp] = i;
+    }
+    
+    for(auto it:mpCnt)
+    {
+        ret.push_back({it.first, it.second});
+    }
+    
+    sort(ret.begin(), ret.end(), cmp);
+    
+    for(auto it: ret)
+    {
+        while(it.second--)
+            cout<<it.first<<' ';
+    }
+    
+    return 0;
 }
