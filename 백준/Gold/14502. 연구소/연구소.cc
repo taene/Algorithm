11782867e802556432660d4e3ae65e486ce69db3
TaeYeonKim <81561694+taene@users.noreply.github.com>
2026-02-24 00:17:12 +0900

    [Gold IV] Title: 연구소, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,100 +1,91 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int N, M;
-int adj[8][8];
-bool visited[8][8];
-vector<pair<int, int>> wall;
-vector<pair<int, int>> virus;
-int dy[] = { -1,1,0,0 };
-int dx[] = { 0,0,-1,1 };
-int ret;
+int N,M,ret;
+int arr[10][10];
+vector<pair<int,int>> canSetWall;
+vector<pair<int,int>> virusStart;
+bool visited[10][10];
+int dy[]={-1,0,1,0};
+int dx[]={0,1,0,-1};
 
-void vir(int y, int x)
+void dfs(int y, int x)
 {
-	visited[y][x] = 1;
-
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-		if (visited[ny][nx]) continue;
-		if (adj[ny][nx] == 1) continue;
-
-		vir(ny, nx);
-	}
-	return;
-}
-
-int solve()
-{
-	fill(&visited[0][0], &visited[0][0] + 8 * 8, 0);
-	for (pair<int, int> i : virus)
-	{
-		//visited[i.first][i.second] = 1;
-		vir(i.first, i.second);
-	}
-	/*for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (adj[i][j] == 2 && !visited[i][j])
-				vir(i, j);
-		}
-	}*/
-
-	int cnt = 0;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (adj[i][j] == 0 && !visited[i][j])
-				cnt++;
-		}
-	}
-	return cnt;
+    visited[y][x] = true;
+    
+    for(int i=0; i<4; ++i)
+    {
+        int ny = y+dy[i];
+        int nx = x+dx[i];
+        
+        if(ny<0||nx<0||ny>=N||nx>=M) continue;
+        if(visited[ny][nx]) continue;
+        if(arr[ny][nx]==1) continue;
+        
+        dfs(ny,nx);
+    }
 }
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			cin >> adj[i][j];
-			if (adj[i][j] == 2) virus.push_back({ i,j });
-			if (adj[i][j] == 0) wall.push_back({ i,j });
-		}
-	}
-
-	for (int i = 0; i < wall.size(); i++)
-	{
-		for (int j = 0; j < i; j++)
-		{
-			for (int k = 0; k < j; k++)
-			{
-				adj[wall[i].first][wall[i].second] = 1;
-				adj[wall[j].first][wall[j].second] = 1;
-				adj[wall[k].first][wall[k].second] = 1;
-				
-				ret = max(ret, solve());
-
-				adj[wall[i].first][wall[i].second] = 0;
-				adj[wall[j].first][wall[j].second] = 0;
-				adj[wall[k].first][wall[k].second] = 0;
-			}
-		}
-	}
-	cout << ret;
-
-	return 0;
+    cin>>N>>M;
+    for(int i=0; i<N; ++i)
+    {
+        for(int j=0; j<M; ++j)
+        {
+            int temp;
+            cin>>temp;
+            arr[i][j] = temp;
+            
+            if(temp == 0)
+            {
+                canSetWall.push_back({i,j});
+            }
+            else if(temp == 2)
+            {
+                virusStart.push_back({i,j});
+            }
+        }
+    }
+    
+    for(int i=0; i<canSetWall.size(); i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            for(int k=0; k<j; k++)
+            {
+                fill(&visited[0][0], &visited[0][0]+100, 0);
+                
+                for(int t=0; t<virusStart.size(); t++)
+                {
+                    arr[canSetWall[i].first][canSetWall[i].second]=1;
+                    arr[canSetWall[j].first][canSetWall[j].second]=1;
+                    arr[canSetWall[k].first][canSetWall[k].second]=1;
+                
+                    dfs(virusStart[t].first, virusStart[t].second);
+                
+                    arr[canSetWall[i].first][canSetWall[i].second]=0;
+                    arr[canSetWall[j].first][canSetWall[j].second]=0;
+                    arr[canSetWall[k].first][canSetWall[k].second]=0;
+                }
+                
+                int cnt = -3;
+                for(int y=0; y<N; y++)
+                {
+                    for(int x=0; x<M; x++)
+                    {
+                        if(arr[y][x]==0 && !visited[y][x])
+                            cnt++;
+                    }
+                }
+                
+                ret = max(ret,cnt);
+            }
+        }
+    }
+    
+    cout<<ret;
+    
+    return 0;
 }
